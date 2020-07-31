816403e4a21aecfd889894b703a17fa10cad6598
HenrySungnamKim <hernrysungnamkim@github.com>
2020-08-01 08:13:25 +0900

    add date-fns

--- src/component/Main/index.js
+++ src/component/Main/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-
+import * as dateFns from "date-fns";
 const index = () => {
-  return <div>Hello world</div>;
+  return <div>{dateFns.format(new Date(), "yyyy-MM-dd")}</div>;
 };
 
 export default index;
