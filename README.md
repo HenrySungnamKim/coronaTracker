24d72cc33d6e2e9e2dca2585767f36b9457e8707
HenrySungnamKim <hernrysungnamkim@github.com>
2020-08-01 07:59:38 +0900

    first-commit. hello world

---
+++ README.md
@@ -0,0 +1,3 @@
+# coronaTracker
+
+## 코로나 바이러스 현황 
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Main from "../src/component/Main";
+const index = () => {
+  return <Main />;
+};
+
+export default index;
